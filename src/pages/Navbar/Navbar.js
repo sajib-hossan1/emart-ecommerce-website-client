853e01c00df9c46816d2c9694dbf69d6265aaa11
sajib-hossan1 <sajib.hossan112@gmail.com>
2023-03-18 20:39:38 +0600

    navbar and footer updated

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -6,18 +6,18 @@ const Navbar = () => {
         <div className='sticky-top'>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container">
-                    <a className="navbar-brand" href="#nav">EMart</a>
-                        <form className="d-flex mx-auto" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                    <a className="nav-brand" href="#nav">EMart</a>
                     <div>
                         <div className="nav-links">
-                            <a className="nav-link" href="#home">Home</a>
-                            <a className="nav-link" href="#products">Products</a>
-                            <a className="nav-link" href="#products"><i className="fa-solid fa-cart-shopping"/> Cart</a>
+                            <a className="nav-link" href="#home">HOME</a>
+                            <a className="nav-link" href="#products">PRODUCTS</a>
+                            <a className="nav-link" href="#products"><i className="fa-solid fa-cart-shopping"/> CART</a>
                         </div>
                     </div>
+                    <form className="d-flex search-form" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
             </nav>
         </div>
